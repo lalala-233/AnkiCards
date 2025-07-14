653cd35daf5f2df7674b26f3debd4280f50ea55d
lalala-233 <lalala_233@qq.com>
2025-07-14 15:08:52 +0800

    break: 几乎所有含 `...` 的单词都增加了空格
    break: 单词中含中文省略号「…」的被修改为英文省略号
    
    feat: 删除一些重复的句号
    
    feat: 初步实现 word 字段的检查

--- New/Eng/project/src/main.rs
+++ New/Eng/project/src/main.rs
@@ -1,90 +1,126 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
-
 fn main() -> std::io::Result<()> {
     // Open the input file (test.txt)
-    let file_name = &std::env::args().nth(1).expect("Please provide a file name");
-    let template = "英语单词模板";
-    let input_file = File::open(file_name)?;
-    let reader = BufReader::new(input_file);
-    // Create or open the output file (result.txt)
-    let mut output_file = File::create(format!("{file_name}-result.txt"))?;
-    impure::write_header(&mut output_file)?;
-    let mut counts = 0.0;
-    // Process each line from the input file
-    for line in reader.lines().map_while(Result::ok) {
-        {
-            // Split the line into parts using the "|" delimiter
+    let file_name = impure::read_env();
+    let binding = impure::read_to_string(&file_name)?;
+    let (_, content) = split_header(&binding);
+    let error_info = todo::check(content);
+    impure::copy_error(error_info, binding.lines().count());
+    Ok(())
+}
 
-            counts += 1.0;
-            let parts: Vec<&str> = line.split('|').collect();
-            if parts.len() != 7 {
-                eprintln!("{parts:?}");
-                impure::copy_error("不是 7 个", parts[4], counts);
-            }
-
-            // Extract the English sentence and Chinese translation
-            let english_sentence: Vec<&str> = parts[4].split("<br>").collect();
-            let chinese_translation: Vec<&str> = parts[5].split("<br>").collect();
-
-            let mut english = english_sentence[0].to_owned();
-            let mut chinese = String::new();
-            if english_sentence.len() != chinese_translation.len() {
-                eprintln!("{english} | {chinese}");
-                impure::copy_error("对不上", parts[4], counts);
-            }
-            if english.is_ascii() {
-                chinese_translation[0].clone_into(&mut chinese);
-            } else {
-                let space_index = english.find(|c: char| !c.is_ascii()).unwrap();
-
-                // Split the string into two parts
-                english_sentence[0][0..space_index].clone_into(&mut english);
-                english_sentence[0][space_index..]
-                    .to_owned()
-                    .clone_into(&mut chinese);
-                eprintln!("{english} | {chinese}");
-                impure::copy_error("已复制", parts[4], counts);
-            }
-
-            let processed_line = format!(
-                "\"{}\"|\"{}\"|\"{}\"|\"{}\"|\"{}\"|\"{}\"|\"{}\"|\"{}\"\n",
-                parts[0], parts[1], parts[2], parts[3], english, chinese, parts[6], template
-            );
-            // Write the processed line to the output file
-
-            write!(output_file, "{processed_line}")?;
+fn split_header(file: &str) -> (&str, &str) {
+    let index = file.match_indices('\n').nth(5).unwrap().0;
+    file.split_at(index + '\n'.len_utf8())
+}
+pub enum Error {}
+mod todo {
+    pub fn check(s: &str) -> Option<(usize, String)> {
+        s.lines()
+            .enumerate()
+            .find_map(|(count_of_line, line)| check_line(line).err().map(|s| (count_of_line, s)))
+    }
+    fn check_line(line: &str) -> Result<(), String> {
+        let Some([
+            _deck,
+            word,
+            _pronunciation,
+            _chinese,
+            _english_sentence,
+            _chinese_sentence,
+            _tag,
+            _notetype,
+        ]) = extract_line(line)
+        else {
+            return Err('|'.to_string());
+        };
+        check_word(word)?;
+        // check_english_sentence(english_sentence)?;
+        Ok(())
+    }
+    fn check_word(word: &str) -> Result<(), String> {
+        if word.chars().all(is_valid_word_char) && is_valid_word_str(word) {
+            return Ok(());
+        }
+        Err(word.to_string())
+    }
+    fn is_valid_word_char(c: char) -> bool {
+        c.is_ascii_alphanumeric() || VALID_WORD_CHAR.contains(&c)
+    }
+    const VALID_WORD_CHAR: &[char] = &['\'', '-', ' ', '(', ')', ',', '.', '?', '!','/'];
+    const ALLOWED_COMBINATIONS: &[&str] = &[
+        " (", ") ", ", ", "' ", /* ones' something */
+        ".."," .",". " /* special handing */
+    ];
+    fn cartesian_product(chars_1: &[char], chars_2: &[char]) -> Vec<String> {
+        chars_1
+            .iter()
+            .flat_map(|c1| chars_2.iter().map(move |c2| format!("{c1}{c2}")))
+            .collect()
+    }
+    fn is_valid_word_str(s: &str) -> bool {
+        is_combination_valid(s) && is_extra_combination_valid(s)
+    }
+    fn is_extra_combination_valid(s: &str) -> bool {
+        if s.contains("..") || s.contains(" .")|| s.contains(". "){
+            s.contains("...")
+        } else {
+            true
         }
     }
+    fn is_combination_valid(s: &str) -> bool {
+        cartesian_product(VALID_WORD_CHAR, VALID_WORD_CHAR)
+            .iter()
+            .filter(|s| !ALLOWED_COMBINATIONS.contains(&s.as_str()))
+            .all(|invalid| !s.contains(invalid))
+    }
+    fn _check_english_sentence(sentence: &str) -> Result<(), String> {
+        let error_list = ["："];
+        if error_list.iter().any(|error| sentence.contains(error)) {
+            return Err(sentence.to_string());
+        }
+        // for error in error_list {
+        //     match_then_err(sentence, error)?;
+        // }
 
-    println!("Processed lines are written to result.txt with {counts}");
-    Ok(())
+        Ok(())
+    }
+    fn extract_line(line: &str) -> Option<[&str;8]> {
+        let mut iter = line.split('|');
+        let result = [
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+            iter.next().unwrap(),
+        ];
+        if iter.next().is_some() {
+            None
+        } else {
+            Some(result)
+        }
+    }
 }
 mod impure {
     use std::{fs::File, io::Write, thread::sleep, time::Duration};
-
-    pub fn write_header(output_file: &mut File) -> Result<(), std::io::Error> {
-        output_file.write_all(
-            "#separator:|
-#html:true
-#notetype column:8
-#deck column:1
-#tags column:7
-"
-            .as_bytes(),
-        )
+    pub fn read_env() -> String {
+        std::env::args().nth(1).expect("Please provide a file name")
     }
-    pub fn copy_error(error_info: &str, copy_content: &str, counts_of_pass: f64) {
-        let all_line = 19403.0;
-        let mut board = arboard::Clipboard::new().unwrap();
-        board.set_text(copy_content).unwrap();
-        sleep(Duration::from_millis(25));
-        panic!(
-            "{}，{}/{}，{}",
-            error_info,
-            counts_of_pass,
-            all_line,
-            counts_of_pass / all_line
-        );
+    pub fn read_to_string(file_name: &str) -> std::io::Result<String> {
+        std::fs::read_to_string(file_name)
+    }
+    pub fn _write_to_file(file_name: &str, header: &str, content: &str) -> std::io::Result<()> {
+        let mut file = File::create(file_name)?;
+        write!(file, "{header}{content}")
+    }
+    pub fn copy_error(error_info: Option<(usize, String)>, len_of_line: usize) {
+        if let Some((line, s)) = error_info {
+            let mut board = arboard::Clipboard::new().unwrap();
+            board.set_text(&s).unwrap();
+            sleep(Duration::from_millis(25));
+            panic!("Invalid `{s}` at {line}/{len_of_line}",);
+        }
     }
 }
